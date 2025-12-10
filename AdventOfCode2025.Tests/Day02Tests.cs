5c47ddc90366a9442f617860066311d526e62be4
Andrew Lobdell <andrew.s.lobdell@gmail.com>
2025-12-10 11:07:14 -0600

    Day 2

--- AdventOfCode2025.Tests/Day02Tests.cs
+++ AdventOfCode2025.Tests/Day02Tests.cs
@@ -12,7 +12,7 @@ public class Day02Tests
         return File.ReadAllText(path);
     }
 
-    [Fact(Skip = "Fill example input/expected files under AdventOfCode2025.Tests/TestData/DayXX before enabling this test.")]
+    [Fact()]
     public void Part1_Example()
     {
         var input = ReadTestData("part1.example.input.txt");
@@ -24,7 +24,7 @@ public class Day02Tests
         Assert.Equal(expected, result);
     }
 
-    [Fact(Skip = "Fill example input/expected files under AdventOfCode2025.Tests/TestData/DayXX before enabling this test.")]
+    [Fact()]
     public void Part2_Example()
     {
         var input = ReadTestData("part2.example.input.txt");
@@ -35,4 +35,41 @@ public class Day02Tests
 
         Assert.Equal(expected, result);
     }
+
+    [Fact()]
+    public void ParsesCorrectRange()
+    {
+        var rangeStr = "100-200";
+        var range = Day02.Range.Parse(rangeStr);
+
+        Assert.Equal("100", range.Start);
+        Assert.Equal("200", range.End);
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidIdTestData))]
+    public void IdentifiesInvalidIdsInRange(string rangeStr, List<long> expectedInvalidIds, List<long> expectedInvalidIdsPart2)
+    {
+        foreach (var part in new[] { 1, 2 })
+        {
+            var range = Day02.Range.Parse(rangeStr);
+            range.GetInvalidInRange(part: part);
+            var expected = part == 1 ? expectedInvalidIds : expectedInvalidIdsPart2;
+            Assert.Equal(expected, range.InvalidIds);
+        }
+    }
+    public static TheoryData<string, List<long>, List<long>> InvalidIdTestData => new()
+    {
+        { "11-22", new List<long> { 11, 22}, new List<long> {11, 22} },
+        { "95-115", new List<long> {99} , new List<long> {99, 111} },
+        { "998-1012", new List<long> {1010}, new List<long> {999, 1010} },
+        { "1188511880-1188511890", new List<long> {1188511885}, new List<long> {1188511885} },
+        { "222220-222224", new List<long> {222222}, new List<long> {222222} },
+        { "1698522-1698528", new List<long> {} , new List<long> {} },
+        { "446443-446449", new List<long> {446446}, new List<long> {446446} },
+        { "38593856-38593862", new List<long> {38593859}, new List<long> {38593859} },
+        {"565653-565659", new List<long> {}, new List<long> {565656} },
+        {"824824821-824824827", new List<long> {}, new List<long> {824824824} },
+        {"2121212118-2121212124", new List<long> {}, new List<long> {2121212121} },
+    };
 }
--- Days/Day02.cs
+++ Days/Day02.cs
@@ -4,13 +4,99 @@ public sealed class Day02 : IDay
 {
     public string SolvePart1(string input)
     {
-        // TODO: implement solution for Part 1
-        throw new NotImplementedException();
+        long invalidSum = 0;
+        string[] ranges = input.Split(',', StringSplitOptions.RemoveEmptyEntries);
+        foreach (var rangeStr in ranges)
+        {
+            var range = Range.Parse(rangeStr);
+            range.GetInvalidInRange();
+            invalidSum += range.SumInvalid();
+        }
+
+        return invalidSum.ToString();
     }
 
     public string SolvePart2(string input)
     {
-        // TODO: implement solution for Part 2
-        throw new NotImplementedException();
+        long invalidSum = 0;
+        string[] ranges = input.Split(',', StringSplitOptions.RemoveEmptyEntries);
+        foreach (var rangeStr in ranges)
+        {
+            var range = Range.Parse(rangeStr);
+            range.GetInvalidInRange(part: 2);
+            invalidSum += range.SumInvalid();
+        }
+
+        return invalidSum.ToString();
+    }
+
+    public class Range
+    {
+        public required string Start { get; set; }
+        public required string End { get; set; }
+        public List<long> InvalidIds = [];
+
+        public static Range Parse(string rangeStr)
+        {
+            var parts = rangeStr.Split('-', StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length != 2)
+                throw new FormatException($"Invalid range format: {rangeStr}");
+
+            return new Range
+            {
+                Start = parts[0],
+                End = parts[1]
+            };
+        }
+
+        public void GetInvalidInRange(int part = 1)
+        {
+            for (long i = long.Parse(Start); i <= long.Parse(End); i++)
+            {
+                if (IsInvalid(i, part))
+                {
+                    InvalidIds.Add(i);
+                }
+            }
+        }
+
+        public long SumInvalid()
+        {
+            return InvalidIds.Sum();
+        }
+
+        private static bool IsInvalid(long id, int part)
+        {
+            var s = id.ToString();
+
+            if (part == 1 && s.Length % 2 != 0) return false;
+            int groupSize = part == 1 ? s.Length / 2 : 1;
+
+            while (groupSize * 2 <= s.Length)
+            {
+                if (s.Length % groupSize != 0)
+                {
+                    groupSize++;
+                    continue;
+                }
+                HashSet<string> set = SplitToSet(s, groupSize);
+                if (set.Count == 1)
+                {
+                    return true;
+                }
+                groupSize++;
+            }
+            return false;
+        }
+    }
+
+    private static HashSet<string> SplitToSet(string input, int size)
+    {
+        HashSet<string> set = [];
+        for (int i = 0; i + size <= input.Length; i += size)
+        {
+            set.Add(input.Substring(i, size));
+        }
+        return set;
     }
 }
